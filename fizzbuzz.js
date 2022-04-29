6025f12504b70e269c080bb0667e9f4600a65130
wezerg <vivian.grimaux@hotmail.fr>
2022-04-29 12:54:01 +0200

    Ok : Affichage des nombre et du résultat

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,13 +3,13 @@ const fizzbuzz = new FizzBuzz();
 
 fizzbuzz.on('calcul', function(number){
     if (number % 5 === 0 && number % 3 === 0) {
-        console.log('FizzBuzz');
+        console.log(number + ' : FizzBuzz');
     }
     else if(number % 5 === 0){
-        console.log('Buzz');
+        console.log(number + ' : Buzz');
     }
     else if(number % 3 === 0){
-        console.log('Fizz');
+        console.log(number + ' : Fizz');
     }
     else{
         console.log(number + " : n'est pas un multiple de 3 et/ou 5");
