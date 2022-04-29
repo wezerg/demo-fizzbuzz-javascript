152d45c094953ee1e1ae9f9f2f5d7f03c1c1387e
wezerg <vivian.grimaux@hotmail.fr>
2022-04-29 12:33:19 +0200

    Création du fichier évènement Rédaction d'un callback pour calculer le multiple du nombre passé en paramètre de l'évènement

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+const FizzBuzz = require('./emitter');
+const fizzbuzz = new FizzBuzz();
+
+fizzbuzz.on('calcul', function(number){
+    if (number % 5 === 0 && number % 3 === 0) {
+        console.log('FizzBuzz');
+    }
+    else if(number % 5 === 0){
+        console.log('Buzz');
+    }
+    else if(number % 3 === 0){
+        console.log('Fizz');
+    }
+    else{
+        console.log(number + " : n'est pas un multiple de 3 et/ou 5");
+    }
+})
