4205e96f9877e8ccd783c7185a11350d240d2cb5
wezerg <vivian.grimaux@hotmail.fr>
2022-04-29 12:50:16 +0200

    OK : Créatin d'un fichier index.js Premier test concluant sur les nombres 1, 5, 10, 15, 17, 21

---
+++ index.js
@@ -0,0 +1,9 @@
+const fizzbuzz = require('./fizzbuzz');
+
+// Appel de l'event
+fizzbuzz.Calcul(1);
+fizzbuzz.Calcul(5);
+fizzbuzz.Calcul(10);
+fizzbuzz.Calcul(15);
+fizzbuzz.Calcul(17);
+fizzbuzz.Calcul(21);
